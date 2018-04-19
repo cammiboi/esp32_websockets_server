6128b77c3dd3707551e471f2e0a6cd87b5a62d91
cammiboi <camjnaw@shaw.ca>
2018-04-18 22:38:31 -0600

    Added websocket

--- main/main.c
+++ main/main.c
@@ -1,19 +1,35 @@
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "lwip/api.h"
+
 #include "esp_wifi.h"
-#include "esp_system.h"
-#include "esp_event.h"
+#include "esp_log.h"
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
+
+#include "esp_system.h"
+#include "esp_event.h"
+
 #include "driver/gpio.h"
+
+#include "string.h"
+
+#include "websocket_server.h"
+
+// set WiFi network name and password to connect to using "make menuconfig"
+#define AP_SSID "The-Commune"
+#define AP_PSSWD "sunnyside"
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     return ESP_OK;
 }
 
-void app_main(void)
-{
-    nvs_flash_init();
+// connects to wifi network
+void setupWiFi(){
+	nvs_flash_init();
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -22,19 +38,184 @@ void app_main(void)
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     wifi_config_t sta_config = {
         .sta = {
-            .ssid = "access_point_name",
-            .password = "password",
+            .ssid = AP_SSID,
+            .password = AP_PSSWD,
             .bssid_set = false
         }
     };
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
     ESP_ERROR_CHECK( esp_wifi_connect() );
+}
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+// websocket variables
+static QueueHandle_t client_queue;
+const static int client_queue_size = 10;
+
+// handles websocket events
+void websocket_callback(uint8_t num,WEBSOCKET_TYPE_t type,char* msg,uint64_t len) {
+  const static char* TAG = "websocket_callback";
+  int value;
+
+  switch(type) {
+    case WEBSOCKET_CONNECT:
+      ESP_LOGI(TAG,"client %i connected!",num);
+      break;
+    case WEBSOCKET_DISCONNECT_EXTERNAL:
+      ESP_LOGI(TAG,"client %i sent a disconnect message",num);
+      //led_duty(0);
+      break;
+    case WEBSOCKET_DISCONNECT_INTERNAL:
+      ESP_LOGI(TAG,"client %i was disconnected",num);
+      break;
+    case WEBSOCKET_DISCONNECT_ERROR:
+      ESP_LOGI(TAG,"client %i was disconnected due to an error",num);
+      //led_duty(0);
+      break;
+    case WEBSOCKET_TEXT:
+      if(len) {
+        switch(msg[0]) {
+          case 'L':
+            if(sscanf(msg,"L%i",&value)) {
+              ESP_LOGI(TAG,"LED value: %i",value);
+              //led_duty(value);
+              ws_server_send_text_all_from_callback(msg,len); // broadcast it!
+            }
+        }
+      }
+      break;
+    case WEBSOCKET_BIN:
+      ESP_LOGI(TAG,"client %i sent binary message of size %i:\n%s",num,(uint32_t)len,msg);
+      break;
+    case WEBSOCKET_PING:
+      ESP_LOGI(TAG,"client %i pinged us with message of size %i:\n%s",num,(uint32_t)len,msg);
+      break;
+    case WEBSOCKET_PONG:
+      ESP_LOGI(TAG,"client %i responded to the ping",num);
+      break;
+  }
+}
+
+//serves clients
+static void http_serve(struct netconn *conn){
+	const static char* TAG = "http_server";
+
+	struct netbuf* inbuf;
+	static char* buf;
+	static uint16_t buflen;
+	static err_t err;
+
+	netconn_set_recvtimeout(conn,1000); // allow a connection timeout of 1 second
+	ESP_LOGI(TAG,"reading from client...");
+	err = netconn_recv(conn, &inbuf);
+	ESP_LOGI(TAG,"read from client");
+	if(err==ERR_OK) {
+		netbuf_data(inbuf, (void**)&buf, &buflen);
+		if(buf) {
+
+				// default page websocket
+			if(strstr(buf,"GET / ")
+				&& strstr(buf,"Upgrade: websocket")) {
+				ESP_LOGI(TAG,"Requesting websocket on /");
+				ws_server_add_client(conn,buf,buflen,"/",websocket_callback);
+				netbuf_delete(inbuf);
+			}
+
+				// not a websocket request
+			else {
+				ESP_LOGI(TAG,"Unknown request");
+				netconn_close(conn);
+				netconn_delete(conn);
+				netbuf_delete(inbuf);
+			}
+		}
+		else {
+			ESP_LOGI(TAG,"Unknown request (empty?...)");
+			netconn_close(conn);
+			netconn_delete(conn);
+			netbuf_delete(inbuf);
+    	}
+	}
+	else { // if err==ERR_OK
+		ESP_LOGI(TAG,"error on read, closing connection");
+		netconn_close(conn);
+		netconn_delete(conn);
+		netbuf_delete(inbuf);
+  	}
+}
+
+// handles clients when they first connect. passes to a queue
+static void server_task(void* pvParameters) {
+  const static char* TAG = "server_task";
+  struct netconn *conn, *newconn;
+  static err_t err;
+  client_queue = xQueueCreate(client_queue_size,sizeof(struct netconn*));
+
+  conn = netconn_new(NETCONN_TCP);
+  netconn_bind(conn,NULL,80);
+  netconn_listen(conn);
+  ESP_LOGI(TAG,"server listening");
+  do {
+    err = netconn_accept(conn, &newconn);
+    ESP_LOGI(TAG,"new client");
+    if(err == ERR_OK) {
+      xQueueSendToBack(client_queue,&newconn,portMAX_DELAY);
+      //http_serve(newconn);
+    }
+  } while(err == ERR_OK);
+  netconn_close(conn);
+  netconn_delete(conn);
+  ESP_LOGE(TAG,"task ending, rebooting board");
+  esp_restart();
+}
+
+// receives clients from queue, handles them
+static void server_handle_task(void* pvParameters) {
+  const static char* TAG = "server_handle_task";
+  struct netconn* conn;
+  ESP_LOGI(TAG,"task starting");
+  for(;;) {
+    xQueueReceive(client_queue,&conn,portMAX_DELAY);
+    if(!conn) continue;
+    http_serve(conn);
+  }
+  vTaskDelete(NULL);
+}
+
+static void count_task(void* pvParameters) {
+  const static char* TAG = "count_task";
+  char out[20];
+  int len;
+  int clients;
+  const static char* word = "%i";
+  uint8_t n = 0;
+  const int DELAY = 1000 / portTICK_PERIOD_MS; // 1 second
+
+  ESP_LOGI(TAG,"starting task");
+  for(;;) {
+    len = sprintf(out,word,n);
+    clients = ws_server_send_text_all(out,len);
+    if(clients > 0) {
+      //ESP_LOGI(TAG,"sent: \"%s\" to %i clients",out,clients);
+    }
+    n++;
+    vTaskDelay(DELAY);
+  }
+}
+
+void app_main(void)
+{
+	setupWiFi();
+
+	ws_server_start();
+  	xTaskCreate(&server_task,"server_task",3000,NULL,9,NULL);
+  	xTaskCreate(&server_handle_task,"server_handle_task",4000,NULL,6,NULL);
+  	xTaskCreate(&count_task,"count_task",6000,NULL,2,NULL);
+
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
     int level = 0;
     while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
+        gpio_set_level(GPIO_NUM_2, level);
         level = !level;
         vTaskDelay(300 / portTICK_PERIOD_MS);
     }
